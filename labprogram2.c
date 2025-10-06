5f5aec2eff8c596a4a3c0e8d6dbaf95b64424588
Sudhanshu Raj <sudhanshuraj.cs24@bmsce.ac.in>
2025-10-06 10:15:40 +0530

    Add files via upload

---
+++ labprogram2.c
@@ -0,0 +1,158 @@
+
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+#define MAX 100
+
+char stack[MAX];
+int top=-1;
+void Push(char c)
+{
+    if(top==MAX-1)
+    {
+        printf("Stack overflow.\n");
+        return;
+    }
+    stack[++top]=c;
+}
+
+char Pop()
+{
+    if(top==-1)
+    {
+        printf("Stack underflow.\n");
+        return -1;
+    }
+    return stack[top--];
+}
+
+char Peek()
+{
+    if(top==-1)
+        return -1;
+    return stack[top];
+}
+
+int precedence(char op)
+{
+    switch(op)
+    {
+        case '+':
+        case '-':
+         return 1;
+        case '*':
+        case '/':
+         return 2;
+        case '(':
+        return 0;
+    }
+    return -1;
+}
+
+int associativity(char op)
+{
+    if(op=='^')
+        return 1;
+    return 0;
+}
+
+// void infixTOPostfix(char infix[],char postfix[])
+// {
+//     int i,k=0;
+//     char c;
+//     for (i=0;infix[i]!='\0';i++)
+//     {
+//         c=infix[i];
+//         if(isalnum(c))
+//         {
+//             postfix[k++]=c;
+//         }
+//         else if(c=='(')
+//         {
+//             Push(c);
+//         }
+//         else if(c==')')
+//         {
+//             while (Peek()!='(')
+//             {
+//                 postfix[k++]=Pop();
+//             }
+//             Pop();
+//         }
+//         else
+//         {
+//             while (top!=-1 && ((precedence(Peek())>precedence(c)) || (precedence(Peek())==precedence(c) &&associativity(c)==0)))
+//             {
+//                 postfix[k++]=Pop();
+//             }  
+//         Push(c);
+//         }
+//     }
+//     while (top!=-1)
+//     {
+//         postfix[k++]=Pop();
+//     }
+//     postfix[k]='\0';
+// }
+void infixTOPostfix(char infix[], char postfix[])
+{
+    int i, k = 0;
+    char c;
+    for (i = 0; infix[i] != '\0'; i++)
+    {
+        c = infix[i];
+        if (isalnum(c))
+        {
+            postfix[k++] = c;
+        }
+        else if (c == '(')
+        {
+            Push(c);
+        }
+        else if (c == ')')
+        {
+            while (top != -1 && Peek() != '(')
+            {
+                postfix[k++] = Pop();
+            }
+            if (top != -1)
+                Pop();
+            else
+            {
+                printf("Error: Mismatched parentheses.\n");
+                return;
+            }
+        }
+        else
+        {
+            while (top != -1)
+            {
+                char top_op = Peek();
+                if (top_op == -1)
+                    break;
+                if ((precedence(top_op) > precedence(c)) || (precedence(top_op) == precedence(c) && associativity(c) == 0))
+                    postfix[k++] = Pop();
+                else
+                    break;
+            }
+            Push(c);
+        }
+    }
+    while (top != -1)
+    {
+        postfix[k++] = Pop();
+    }
+    postfix[k] = '\0';
+}
+
+int main()
+{
+    char infix[MAX],postfix[MAX];
+    printf("Enter a valid parenthesized infix expression:");
+    scanf(" %s",infix);
+    infixTOPostfix(infix,postfix);
+    printf("Postfix expression: %s\n",postfix);
+    return 0;
+}
+
+
